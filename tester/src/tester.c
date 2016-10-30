ca9d44a3057c7ae6478faddf42c5cab491cfbf25
kevin-kan <kevinkan939@gmail.com>
2016-10-30 14:28:28 -0400

    Project 2

---
+++ tester/src/tester.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : tester.c
+ Author      : Kevin Kan
+ Version     :
+ Copyright   : 2016
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
