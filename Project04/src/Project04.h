cac42c03af7d01dc9384a040797283eee8a6ec20
kevin-kan <kevinkan939@gmail.com>
2016-11-21 23:50:01 -0500

    Project 4 buggy compiler?

---
+++ Project04/src/Project04.h
@@ -0,0 +1,19 @@
+/*
+ * Project04.h
+ *
+ *  Created on: Nov 21, 2016
+ *      Author: Kevin
+ */
+
+#ifndef PROJECT04_H_
+#define PROJECT04_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void FileTest(int bufferSize);
+
+#endif /* PROJECT04_H_ */
